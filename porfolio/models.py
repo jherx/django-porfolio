a0691228be878fed26c2f939db0c4d3dc90bb8fb
Gerson Santos <jhxn07@gmail.com>
2023-06-25 11:25:07 -0600

    proyecto de django

---
+++ porfolio/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db.models.fields import CharField, URLField
+from django.db.models.fields.files import ImageField
+
+
+class Project(models.Model):
+    title = CharField(max_length=100)
+    descripcion = CharField(max_length=150)
+    image = ImageField(upload_to='porfolio/images')
+    url = URLField(blank=True)
